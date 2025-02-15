9e89ae103307091210588dc77bc8160a5bcdc463
kacygd <164707129+kacygd@users.noreply.github.com>
2025-02-15 19:58:41 +0700

    Update ListManager.h

--- src/ListManager.h
+++ src/ListManager.h
@@ -7,82 +7,88 @@
 using namespace geode::prelude;
 
 class ListManager {
-    public:
-        inline static std::vector<int> demonIDList;
-        inline static bool firstTimeOpen;
-        inline static int filterType;
-        inline static bool isSupremeSearching;
+public:
+    inline static std::vector<int> demonIDList;
+    inline static bool firstTimeOpen;
+    inline static int filterType;
+    inline static bool isSupremeSearching;
 
-        inline static void parseRequestString(std::string str) {
-            size_t isFound = str.find("_id");
+    inline static void parseRequestString(std::string str) {
+        size_t isFound = str.find("_id");
 
-            while (isFound != std::string::npos) {
-                str = str.substr(isFound + 5);
-                size_t findBracket = str.find("}");
+        while (isFound != std::string::npos) {
+            str = str.substr(isFound + 5);
+            size_t findBracket = str.find("}");
 
-                if (findBracket == std::string::npos) break; // Kiểm tra tránh lỗi out of range
+            if (findBracket == std::string::npos) break;  // Tránh lỗi nếu không tìm thấy dấu '}'
 
-                int id = stoi(str.substr(0, findBracket));
+            try {
+                int id = std::stoi(str.substr(0, findBracket));
                 demonIDList.push_back(id);
-
-                isFound = str.find("_id");
+            } catch (const std::exception& e) {
+                break; // Tránh lỗi khi `stoi` thất bại
             }
+
+            isFound = str.find("_id");
         }
+    }
 
-        inline static int getPositionOfID(int id) {
-            for (size_t i = 0; i < demonIDList.size(); i++) {
-                if (demonIDList[i] == id) return static_cast<int>(i);
-            }
-            return -1;
+    inline static int getPositionOfID(int id) {
+        for (unsigned int i = 0; i < demonIDList.size(); i++) {
+            if (demonIDList[i] == id) return i;
         }
+        return -1;
+    }
 
-        inline static CCSprite* getSpriteFromPosition(int pos, bool hasText) {
-            if (pos <= 499 && pos > 249) {
-                return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon0_text.png"_spr : "GrD_demon0.png"_spr);
-            }
-            if (pos <= 249 && pos > 149) {
-                return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon1_text.png"_spr : "GrD_demon1.png"_spr);
-            }
-            if (pos <= 149 && pos > 74) {
-                return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon2_text.png"_spr : "GrD_demon2.png"_spr);
-            }
-            if (pos <= 74 && pos > 24) {
-                return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon3_text.png"_spr : "GrD_demon3.png"_spr);
-            }
-            if (pos <= 24 && pos > 0) {
+    inline static CCSprite* getSpriteFromPosition(int pos, bool hasText) {
+        if (pos <= 499 && pos > 249) {
+            return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon0_text.png"_spr : "GrD_demon0.png"_spr);
+        }
+        if (pos <= 249 && pos > 149) {
+            return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon1_text.png"_spr : "GrD_demon1.png"_spr);
+        }
+        if (pos <= 149 && pos > 74) {
+            return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon2_text.png"_spr : "GrD_demon2.png"_spr);
+        }
+        if (pos <= 74 && pos > 24) {
+            return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon3_text.png"_spr : "GrD_demon3.png"_spr);
+        }
+        if (pos <= 24 && pos > 0) {
+            return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon4_text.png"_spr : "GrD_demon4.png"_spr);
+        }
+        if (pos == 0) {
+            if (Mod::get()->getSettingValue<bool>("grandpa-demon-disable")) {
                 return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon4_text.png"_spr : "GrD_demon4.png"_spr);
-            }
-            if (pos == 0) {
-                if (Mod::get()->getSettingValue<bool>("grandpa-demon-disable")) {
-                    return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon4_text.png"_spr : "GrD_demon4.png"_spr);
-                } else {
-                    return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon5_text.png"_spr : "GrD_demon5.png"_spr);
-                }
-            }
-            return nullptr; // Giá trị mặc định nếu không thỏa điều kiện
-        }
-
-        inline static GJSearchObject* getSearchObject(int upper, int lower) {
-            if (demonIDList.empty()) return nullptr; // Tránh lỗi khi danh sách trống
-
-            std::stringstream download;
-            bool first = true;
-
-            if (upper < static_cast<int>(demonIDList.size()) && lower >= 0) {
-                for (int i = upper; i > lower; i--) {
-                    if (i >= static_cast<int>(demonIDList.size())) continue; // Tránh out of range
-
-                    if (!first) download << ",";
-                    download << std::to_string(demonIDList[i]);
-                    first = false;
-                }
             } else {
-                download << std::to_string(demonIDList[0]);
+                return CCSprite::createWithSpriteFrameName(hasText ? "GrD_demon5_text.png"_spr : "GrD_demon5.png"_spr);
             }
-
-            download << "&gameVersion=22";
-            return GJSearchObject::create(SearchType::Type19, download.str());
         }
+        return nullptr; // Trả về nullptr nếu không có điều kiện nào khớp
+    }
+
+    inline static GJSearchObject* getSearchObject(int upper, int lower) {
+        if (upper >= static_cast<int>(demonIDList.size()) || lower < 0) return nullptr; // Kiểm tra phạm vi hợp lệ
+        
+        std::stringstream download;
+        bool first = true;
+
+        if (!(upper == 0 && lower == 0)) {
+            for (int i = upper; i > lower; i--) {
+                if (i >= static_cast<int>(demonIDList.size())) continue; // Tránh truy cập ngoài phạm vi
+
+                if (!first) download << ",";
+                download << std::to_string(demonIDList[i]);
+                first = false;
+            }
+        } else if (!demonIDList.empty()) {
+            download << std::to_string(demonIDList[0]);
+        } else {
+            return nullptr; // Tránh lỗi khi demonIDList rỗng
+        }
+
+        download << "&gameVersion=22";
+        return GJSearchObject::create(SearchType::Type19, download.str());
+    }
 };
 
 #endif
