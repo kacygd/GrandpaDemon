030d6c31a51fd1da973fad456044db81f0c48696
kacygd <164707129+kacygd@users.noreply.github.com>
2025-02-16 17:42:57 +0700

    Update ListManager.h

--- src/ListManager.h
+++ src/ListManager.h
@@ -1,11 +1,7 @@
 #ifndef LISTMANAGER_H
 #define LISTMANAGER_H
-
 #include <vector>
 #include <string>
-#include <sstream>
-#include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -18,11 +14,14 @@ class ListManager {
 
         inline static void parseRequestString(std::string str) {
             size_t isFound = str.find("_id");
+
             while (isFound != std::string::npos) {
                 str = str.substr(isFound + 5);
                 size_t findBracket = str.find("}");
+
                 int id = stoi(str.substr(0, findBracket));
                 demonIDList.push_back(id);
+
                 isFound = str.find("_id");
             }
         }
@@ -31,40 +30,68 @@ class ListManager {
             for (unsigned int i = 0; i < demonIDList.size(); i++) {
                 if (demonIDList.at(i) == id) return i;
             }
+
             return -1;
         }
 
         inline static CCSprite* getSpriteFromPosition(int pos, bool hasText) {
-            if (pos <= 499 && pos > 249) {
-                return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon0_text.png"_spr)
-                               : CCSprite::createWithSpriteFrameName("GrD_demon0.png"_spr);
-            }
-            if (pos <= 249 && pos > 149) {
-                return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon1_text.png"_spr)
-                               : CCSprite::createWithSpriteFrameName("GrD_demon1.png"_spr);
-            }
-            if (pos <= 149 && pos > 74) {
-                return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon2_text.png"_spr)
-                               : CCSprite::createWithSpriteFrameName("GrD_demon2.png"_spr);
-            }
-            if (pos <= 74 && pos > 24) {
-                return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon3_text.png"_spr)
-                               : CCSprite::createWithSpriteFrameName("GrD_demon3.png"_spr);
-            }
-            if (pos <= 24 && pos > 0) {
-                return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon4_text.png"_spr)
-                               : CCSprite::createWithSpriteFrameName("GrD_demon4.png"_spr);
-            }
-            if (pos == 0) {
-                if (Mod::get()->getSettingValue<bool>("grandpa-demon-disable")) {
-                    return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon4_text.png"_spr)
-                                   : CCSprite::createWithSpriteFrameName("GrD_demon4.png"_spr);
-                } else {
-                    return hasText ? CCSprite::createWithSpriteFrameName("GrD_demon5_text.png"_spr)
-                                   : CCSprite::createWithSpriteFrameName("GrD_demon5.png"_spr);
+            if (pos <= 499 &&
+                pos > 249) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon0_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon0.png"_spr);
+                    }
                 }
-            }
-            return CCSprite::create();
+
+            if (pos <= 249 &&
+                pos > 149) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon1_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon1.png"_spr);
+                    }
+                }
+
+            if (pos <= 149 &&
+                pos > 74) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon2_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon2.png"_spr);
+                    }
+                }
+            if (pos <= 74 &&
+                pos > 24) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon3_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon3.png"_spr);
+                    }
+                }
+            if (pos <= 24 &&
+                pos > 0) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon4_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon4.png"_spr);
+                    }
+                }
+            if (pos == 0) {
+                if ((Mod::get()->getSettingValue<bool>("grandpa-demon-disable"))) {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon4_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon4.png"_spr);
+                    }
+                } else {
+                    if (hasText) {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon5_text.png"_spr);
+                    } else {
+                        return CCSprite::createWithSpriteFrameName("GrD_demon5.png"_spr);
+                    }
+                }
+            } 
         }
 
         inline static GJSearchObject* getSearchObject(int upper, int lower) {
@@ -85,7 +112,9 @@ class ListManager {
             download << "&gameVersion=22";
             GJSearchObject* searchObj = GJSearchObject::create(SearchType::Type19, download.str());
             return searchObj;
-        }
+        }   
+
+        
 };
 
 #endif
